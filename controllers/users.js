efbdb6cc2f6442ec17b188ea133b005f40fcfbe7
bryson189 <bryson189@gmail.com>
2019-04-04 19:09:22 -0600

    Completes backend forgot password functionality

--- controllers/users.js
+++ controllers/users.js
@@ -5,10 +5,11 @@ var jwt = require('jsonwebtoken');
 var crypto = require('crypto');
 var bcrypt = require('bcrypt');
 var nodemailer = require('nodemailer');
+var sgTransport = require('nodemailer-sendgrid-transport');
 
 module.exports = {
     initRoutes: function (app, dbClient) {
-        app.post('/createUser', function (req, res) {
+        app.post('/register', function (req, res) {
             dbClient.collection('users').findOne({ email: req.body.email }, function(err, user) {
                 if (err) {
                     res.status(404).json(err);
@@ -21,8 +22,8 @@ module.exports = {
                 }
                 if (!user) //Ensure user does not already exist
                 {
-                    let saltRounds = constants.BCRYPT_SALT_ROUNDS; //CPU intensity varies greatly based on this value
-                    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+                    //CPU intensity varies greatly based on this value
+                    bcrypt.hash(req.body.password, constants.BCRYPT_SALT_ROUNDS, function(err, hash) {
                         // Enroll new user
                         let userObject = {
                             name: req.body.socket_username,
@@ -44,17 +45,17 @@ module.exports = {
                                 res.json(err);
                             } else {
                                 let token = jwt.sign({
-                                                        email: req.body.email,
-                                                        name: req.body.socket_username },
-                                                       constants.JSON_KEY,
-                                                        {
-                                                            expiresIn: constants.USER_TIMEOUT
-                                                        }
-                                                    ); //expire in 30 mins
+                                    email: req.body.email,
+                                    name: req.body.socket_username },
+                                    constants.JSON_KEY,
+                                    {
+                                        expiresIn: constants.USER_TIMEOUT
+                                    }
+                                ); //expire in 30 mins
                                 res.status(200).json({
-                                                        auth_token: token,
-                                                        timeout: constants.USER_TIMEOUT
-                                                    });
+                                    auth_token: token,
+                                    timeout: constants.USER_TIMEOUT
+                                });
                             }
                         });
                     });
@@ -72,142 +73,136 @@ module.exports = {
                 if(user){
                     res.status(200);
                     let token = jwt.sign({
-                                            email: user.email,
-                                            name: user.socket_username
-                                         },
-                                           constants.JSON_KEY,
-                                            {
-                                                expiresIn: constants.USER_TIMEOUT
-                                            }
-                                        ); //expire in 30 mins
-                    res.status(200).json({
-                                            auth_token: token,
-                                            timeout: constants.USER_TIMEOUT
-                                        });
-                }
-                // If user is not found
-                else {
-                    res.status(401).json(info);
-                }
-            })(req,res);
-        });
+                        email: user.email,
+                        name: user.socket_username
+                    },
+                    constants.JSON_KEY,
+                    {
+                        expiresIn: constants.USER_TIMEOUT
+                    }
+                ); //expire in 30 mins
+                res.status(200).json({
+                    auth_token: token,
+                    timeout: constants.USER_TIMEOUT
+                });
+            }
+            // If user is not found
+            else {
+                res.status(401).json(info);
+            }
+        })(req,res);
+    });
 
-        app.post('/forgot', function(req, res, next) {
-            dbClient.collection('users').findOne({ email: req.body.email }, function(err, user) {
-                if (err) {
-                    res.status(404).json(err);
-                    return;
-                }
-                if (!user) //Ensure user does not already exist
-                {
-                    res.json("Error: No account with that e-mail exists!");
-                    return;
-                }
-                else
-                {
-                    crypto.randomBytes(20, function(err, buf) {
-                        if (err) {
-                            res.status(404).json(err);
-                            return;
-                        }
-                        else {
-                            var token = buf.toString('hex');
-                            user.resetPasswordToken = token;
-                            user.resetPasswordExpires = Date.now() + 3600000;
-                            dbClient.collection('users').replaceOne({ email: user.email }, user, sendPasswordReset(req, res, user));
-                        }
-                    });
-                }
-            });
-        });
-
-        app.post('/reset/:token', function(req, res) {
-            dbClient.collection('users').findOne({ email: req.body.email, resetPasswordToken: req.params.token }, function(err, user) {
-                if (err) {
-                    res.status(404).json(err);
-                    return;
-                }
-                if (!user) //Ensure user does not already exist
-                {
-                    res.json("Error: Incorrect email and reset token combination!");
-                    return;
-                }
-                else
-                {
-                    let saltRounds = constants.BCRYPT_SALT_ROUNDS; //CPU intensity varies greatly based on this value
-                    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-                        user.password = hash;
-                        user.resetPasswordToken = undefined;
-                        user.resetPasswordExpires = undefined;
-                        dbClient.collection('users').replaceOne({ email: user.email }, user, notifyPasswordChange(req, res, user));
-                    });
-                }
-            });
-        });
-
-        app.get('/protected', jwtExpress({secret: constants.JSON_KEY}), function(req, res){
-                if (!req.user.admin) return res.sendStatus(401);
-                res.sendStatus(200);
-        });
-
-        app.use(function (err, req, res, next) {
-            if (err.name === 'UnauthorizedError') {
-                res.status(401).send('Invalid Token Sent');
+    app.post('/forgot', function(req, res, next) {
+        dbClient.collection('users').findOne({ email: req.body.email }, function(err, user) {
+            if (err) {
+                res.status(404).json(err);
+                return;
+            }
+            if (!user) //Ensure user does not already exist
+            {
+                res.json("Error: No account with that e-mail exists!");
+                return;
+            }
+            else
+            {
+                crypto.randomBytes(20, function(err, buf) {
+                    if (err) {
+                        res.status(404).json(err);
+                        return;
+                    }
+                    else {
+                        var token = buf.toString('hex');
+                        user.resetPasswordToken = token;
+                        user.resetPasswordExpires = Date.now() + 3600000;
+                        dbClient.collection('users').replaceOne({ email: user.email }, user, sendPasswordReset(req, res, user));
+                    }
+                });
             }
         });
-    }
+    });
+
+    app.post('/reset/:token', function(req, res) {
+        dbClient.collection('users').findOne({ resetPasswordToken: req.params.token }, function(err, user) {
+            if (err) {
+                res.status(404).json(err);
+                return;
+            }
+            if (!user) //Ensure user does not already exist
+            {
+                res.json("Error: Incorrect email and reset token combination!");
+                return;
+            }
+            else
+            {
+                //CPU intensity varies greatly based on this value
+                bcrypt.hash(req.body.password, constants.BCRYPT_SALT_ROUNDS, function(err, hash) {
+                    user.password = hash;
+                    user.resetPasswordToken = undefined;
+                    user.resetPasswordExpires = undefined;
+                    dbClient.collection('users').replaceOne({ email: user.email }, user, notifyPasswordChange(req, res, user));
+                });
+            }
+        });
+    });
+
+    // app.get('/protected', jwtExpress({secret: constants.JSON_KEY}), function(req, res){
+    //         if (!req.user.admin) return res.sendStatus(401);
+    //         res.sendStatus(200);
+    // });
+    //
+    // app.use(function (err, req, res, next) {
+    //     if (err.name === 'UnauthorizedError') {
+    //         res.status(401).send('Invalid Token Sent');
+    //     }
+    // });
+}
 }
 
 function sendPasswordReset(req, res, user) {
-        let smtpTransport = nodemailer.createTransport({
-          service: 'SendGrid',
-          auth: {
+    let smtpTransport = nodemailer.createTransport(sgTransport({
+        auth: {
             api_key: constants.SEND_GRID_API_KEY
-          }
-        });
-        let mailOptions = {
-          to: user.email,
-          from: 'passwordreset@NetChatter.com',
-          subject: 'Net Chatter Password Reset',
-          text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-            'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-            'http://' + req.headers.host + '/reset/' + user.resetPasswordToken + '\n\n' +
-            'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-        };
-        smtpTransport.sendMail(mailOptions, function(err) {
-            if (err) {
-                console.log("Error! Unable to send out reset email: " + err)
-            }
-            else {
-                res.sendStatus(200).json({
-                    email: user.email
-                });
-            }
-        });
+        }
+    }));
+    let mailOptions = {
+        to: user.email,
+        from: 'passwordreset@NetChatter.com',
+        subject: 'Net Chatter Password Reset',
+        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+        'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+        'http://' + req.headers.host + '/reset/' + user.resetPasswordToken + '\n\n' +
+        'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+    };
+    smtpTransport.sendMail(mailOptions, function(err) {
+        if (err) {
+            console.log("Error! Unable to send out reset email: " + err)
+        }
+        else {
+            res.sendStatus(200);
+        }
+    });
 }
 
 function notifyPasswordChange(req, res, user) {
-        let smtpTransport = nodemailer.createTransport('SMTP', {
-          service: 'SendGrid',
-          auth: {
-              api_key: constants.SEND_GRID_API_KEY
-          }
-        });
-        let mailOptions = {
-          to: user.email,
-          from: 'passwordreset@NetChatter.com',
-          subject: 'Your password has been changed',
-          text: 'Hello,\n\n' +
-            'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
-        };
-        smtpTransport.sendMail(mailOptions, function(err) {
-            if (err) {
-                console.log("Error! Unable to send out password change email: " + err)
-            }
-            else {
-                res.sendStatus(200).json({
-                    email: user.email
-                });
-            }
-        });
+    let smtpTransport = nodemailer.createTransport(sgTransport({
+        auth: {
+            api_key: constants.SEND_GRID_API_KEY
+        }
+    }));
+    let mailOptions = {
+        to: user.email,
+        from: 'passwordreset@NetChatter.com',
+        subject: 'Your password has been changed',
+        text: 'Hello,\n\n' +
+        'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+    };
+    smtpTransport.sendMail(mailOptions, function(err) {
+        if (err) {
+            console.log("Error! Unable to send out password change email: " + err)
+        }
+        else {
+            res.sendStatus(200);
+        }
+    });
 }
