0e156e9d13e7cd294a0507c9490c5c8ef40a7c23
alexander19lam <alexander.lam@ucalgary.ca>
2019-04-04 21:05:27 -0600

    Added Grouping to Chat Bar
    Added Collasable groups to the sidebar
    changed messages to only use dangerous html during search

--- public/components/SideArea/Chats.js
+++ public/components/SideArea/Chats.js
@@ -4,7 +4,20 @@ export default function Chats(props) {
 
     let chosen = props.chosenChat && props.chosenChat.id;
     // console.log(props);
-    const chats = props.chats.map((d,i) => {
+    const style = {width:'100%'};
+
+    const groupChats = props.chats.filter(chat => chat.group).map((d,i) => {
+        return (
+            //${d.group ? 'alert-info' : 'alert-secondary' }
+            <li key={i} className={`list-group-item ${chosen == d.id ? 'list-group-item-action active' : 'list-group-item-action' } `} 
+                onClick={() => {console.log(d.group);props.chooseChat(d)}}
+                >
+                {d.name}
+            </li>
+        )
+    });
+
+    const singleChats = props.chats.filter(chat => !chat.group).map((d,i) => {
         return (
             //${d.group ? 'alert-info' : 'alert-secondary' }
             <li key={i} className={`list-group-item ${chosen == d.id ? 'list-group-item-action active' : 'list-group-item-action' } `} 
@@ -17,11 +30,32 @@ export default function Chats(props) {
 
     return (
         <div className='row'>
-            <div className='col-12'>
-                <ul className='list-group-flush text-center'>
-                    {chats}
+            {/* Only Render when there is Elements */}
+            {!groupChats.length ? '' :
+            <div className='col-12 card' style={style}>
+                <h5 className="card-header text-center" 
+                    data-toggle="collapse" data-target="#collapseGroupChat" 
+                    aria-expanded="true" aria-controls="collapseGroupChat">
+                    Group Chats
+                </h5>
+                <ul id='collapseGroupChat' className='list-group-flush text-center collapse show'>
+                    {groupChats}
                 </ul>
             </div>
+            }
+            {/* Only Render when there is Elements */}
+            {!singleChats.length ? '' :
+            <div className='col-12 card' style={style}>
+                <h5 className="card-header text-center"
+                    data-toggle="collapse" data-target="#collapseSingleChat" 
+                    aria-expanded="true" aria-controls="collapseSingleChat">
+                    Individual Chats
+                </h5>
+                <ul id='collapseSingleChat' className='list-group-flush text-center collapse show'>
+                    {singleChats}
+                </ul>
+            </div>
+            }
         </div>
     )
 }
