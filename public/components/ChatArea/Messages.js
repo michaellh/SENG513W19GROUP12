8dbefee0112ab50836c46d53f05e956c928d25f9
alexander19lam <alexander.lam@ucalgary.ca>
2019-04-07 01:55:00 -0600

    Formatted Messages
    Re-did how messages were formatted
    Messages now have menu on click
    Could react, edit, and delete

--- public/components/ChatArea/Messages.js
+++ public/components/ChatArea/Messages.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import MessageUnit from './MessageUnit';
 
 export default class Messages extends Component {
     constructor(props) {
@@ -9,76 +10,19 @@ export default class Messages extends Component {
         }
 
         this.endMessageRef = React.createRef();
-
-        this.fmtDate = this.fmtDate.bind(this);
-        this.handleReact = this.handleReact.bind(this);
-        this.handleDelete = this.handleDelete.bind(this);
-        this.handleEdit = this.handleEdit.bind(this);
-        this.filterMessage = this.filterMessage.bind(this);
         this.scrollToBottom = this.scrollToBottom.bind(this);
-
     }
 
     scrollToBottom(){
         this.endMessageRef.current.scrollIntoView({ behavior: 'smooth' });
     }
-    
-    fmtDate(date){
-
-        const time = `${date.getHours() < 10 ? '0'+date.getHours() : date.getHours()}:${date.getMinutes() < 10 ? '0'+date.getMinutes() : date.getMinutes()}`;
-        return `${date.toLocaleDateString()} ${time}`;
-    }    
-    // console.log(props);
-
-    handleReact(date, userID, reactions, incReaction){
-        this.props.socket.emit('messageReact', {chatID : this.props.chat.id, userID, date, reactions, incReaction});
-    }
-
-    handleDelete(date, userID){
-        this.props.socket.emit('messageDelete', {chatID : this.props.chat.id, userID, date})
-    }
-
-    handleEdit(date, userID){
-        // Message to be edited to
-        let message = 'boo';
-        this.props.socket.emit('messageEdit', {chatID : this.props.chat.id, userID, date, message})
-    }
-
-    filterMessage(searchTerm, message){
-       return searchTerm ? message.split(searchTerm).join(`<span style='background-color:yellow'>${searchTerm}</span>`) : message
-    };
 
     render() {
-        const messages = this.props.messages.map((d,i) => {
-            const isSelf = d.userID == this.props.user.id;
+        const messages = this.props.messages.map((message,i) => {
+            const isSelf = message.userID == this.props.user.id;
             return (
                 <div key={i} className={`row ${isSelf ? 'justify-content-end' : 'justify-content-start'}`} style={{textAlign: isSelf ? 'right':'left'}}>
-                    <div className={`alert alert-primary m-2 ${isSelf ? 'alert-primary' : 'alert-info'}`}>
-                        <div className='text-secondary'>
-                            <small>{this.fmtDate(d.date)} | {d.userName}</small>
-                        </div>
-                        <div>
-                            {this.props.searchTerm ? <span dangerouslySetInnerHTML={{__html:this.filterMessage(this.props.searchTerm, d.message)}}></span> : d.message}
-                        </div>
-                        
-                        {/* {d.message} */}
-                        {
-                            // SHow reaction div if there is a reaction
-                            d.reactions ? 
-                            <div className='alert alert-secondary'>
-                                {`[like:${d.reactions.like},${d.reactions.dislike}]`}
-                            </div>
-                            :
-                            ''
-                        }
-                        <button onClick={() => this.handleReact(d.date, d.userID, d.reactions,'like')}>Like</button>
-                        <button onClick={() => this.handleReact(d.date, d.userID, d.reactions,'dislike')}>Dislike</button>
-                        <button onClick={() => this.handleDelete(d.date, d.userID)}>delete</button>
-                        <button onClick={() => this.handleEdit(d.date, d.userID, d.message)}>Edit</button>
-    
-                        {/* <button className='btn btn-outline-primary' id={`message${i}`}><i className='fas fa-search'></i></button>
-                        {createPopover(`message${i}`, <Reactions id={`message${i}`} />, {placement:'bottom'})} */}
-                    </div>
+                    <MessageUnit socket={this.props.socket} chatID={this.props.chat.id} searchTerm={this.props.searchTerm} message={message} isSelf={isSelf} index={i} />
                 </div>
             )
         });
