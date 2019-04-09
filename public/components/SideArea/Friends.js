cbcf114be1ba8308d2a9ef8e7e1a0b76ba88fa56
Martin Tran <martin.hua.tran@gmail.com>
2019-04-09 16:52:45 +0000

    clicking on friend now opens 1:1 chat

--- public/components/SideArea/Friends.js
+++ public/components/SideArea/Friends.js
@@ -1,20 +1,45 @@
 import React from 'react'
 
 export default function Friends(props) {
+
+    const singleChats = props.chats.filter(chat => !chat.group);
+    const chosenChatNames = props.chosenChat.map(chat => chat && chat.members.map(member => member.name));
+    
+    let handleOnClick = (friend) => {
+	let friendChat = singleChats.filter(chat => chat.name == friend);
+	if (friendChat.length){
+	    if (chosenChatNames[0] && chosenChatNames[0].includes(friendChat[0].name)){
+		props.resetChat(1);
+	    }
+	    else if(chosenChatNames[1] && chosenChatNames[1].includes(friendChat[0].name)){
+		props.resetChat(2);
+	    }
+	    else{
+		props.chooseChat(friendChat[0]);
+	    }
+	}
+	else{
+	    props.socket.emit('createSingleChat', friend);
+	}
+    }
     
     const friends = props.friends.map((d,i) => {
         return (
-            <li key={i} className='list-group-item text-primary'>{d}</li>
+		<li key={i} className={`list-group-item ${chosenChatNames.some(nameList => nameList && nameList.includes(d)) ? 'list-group-item-action active' : 'list-group-item-action' } `}
+	    onClick={() => {handleOnClick(d)}}
+		>
+		{d}
+	    </li>
         )
     });
 
     return (
-        <div className='row'>
+            <div className='row'>
             <div className='col-12'>
-                <ul className='list-group-flush text-center'>
-                    {friends}
-                </ul>
+            <ul className='list-group-flush text-center'>
+            {friends}
+        </ul>
             </div>
-        </div>
+            </div>
     )
 }
